98f5e170fb574b8b5b53fe206f0055d6681e64e4
fgcintra <fausto@fgcintra.pro.br>
2017-08-14 11:05:22 -0300

    Views, rotas e ações de controller adicionais (14/08)

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -5,6 +5,17 @@ module.exports = function() {
    controller.index = function(req, res) {
       res.render('index', {nome: 'Testando123'});
    }
+
+   controller.ajuda = function(req, res) {
+      res.render('ajuda'); // Sem variável na view
+   }
+
+   controller.login = function(req, res) {
+      res.render('login', {
+         nome: 'Testando123',
+         mensagem: 'Informe seus dados para ter acesso ao sistema'
+      }); // Duas variáveis na view
+   }
    
    return controller;
 
--- app/routes/home.js
+++ app/routes/home.js
@@ -3,4 +3,8 @@ var c = controller();
 
 module.exports = function(app) {
    app.get('/', c.index);
+   app.get('/index', c.index);
+   app.get('/ajuda', c.ajuda);
+   app.get('/login', c.login);
+   app.get('/help', c.ajuda);
 };
