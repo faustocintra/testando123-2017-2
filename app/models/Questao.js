d62dac581b105709630f3604b5f8b092ef581af1
fgcintra <fausto@fgcintra.pro.br>
2017-08-24 09:12:34 -0300

    Instalação do mongoose e criação do primeiro modelo (24/08)

---
+++ app/models/Questao.js
@@ -0,0 +1,18 @@
+var mongoose = require('mongoose');
+
+module.exports = function() {
+
+   var schema = mongoose.Schema({
+      descricao: {
+         type: String,
+         required: true
+      },
+      categoria: {
+         type: String,
+         required: true
+      }
+   });
+
+   return mongoose.model('Questao', schema);
+
+}
