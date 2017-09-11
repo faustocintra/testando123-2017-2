a1a76f8c794eb011b29c933fcbb1cce99589ab7b
fgcintra <fausto@fgcintra.pro.br>
2017-09-11 11:09:40 -0300

    Instalação do Angular 4 e criação do projeto front (11/09)

---
+++ front/src/app/lista/lista.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lista',
+  templateUrl: './lista.component.html',
+  styleUrls: ['./lista.component.css']
+})
+
+export class ListaComponent implements OnInit {
+
+  categorias = ['Computação', 'Administração', 'Economia',
+    'Contabilidade', 'Engenharia de Software']
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
